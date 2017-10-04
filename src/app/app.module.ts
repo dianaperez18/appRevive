85afd00419545873d9bbcc4d5212fa0051ad923f
dianaperez18 <nanis1122013@gmail.com>
2017-10-04 18:29:32 -0500

    Modulo de cursos y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 
 import { ClientesModule } from './clientes/clientes.module';
+import { CursosModule } from './cursos/cursos.module';
+
+
 import { MenuComponent } from './menu/menu.component';
 import { OpcionSeleccionadaComponent } from './opcion-seleccionada/opcion-seleccionada.component';
+
+import { IndexComponent as IndexClientesComponent } from './clientes/index/index.component';
+import { IndexComponent as IndexCursosComponent  } from './cursos/index/index.component';
+
+
+const appRoutes:Routes = [
+
+  {path:"clientes/index", component: IndexClientesComponent },
+  {path:"cursos/index", component: IndexCursosComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +30,9 @@ import { OpcionSeleccionadaComponent } from './opcion-seleccionada/opcion-selecc
   ],
   imports: [
     BrowserModule,
-    ClientesModule
+    ClientesModule,
+    CursosModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
